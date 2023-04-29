750e980a210b257a7446c79b7e1ab671d0a7c130
bestfarmer94 <sjasjaruddus@naver.com>
2023-04-29 11:11:52 +0900

    fix: Selenium 주석 처리. 광고 공간 추가.

--- src/main/java/com/sparta/project_d/selenium/Selenium.java
+++ src/main/java/com/sparta/project_d/selenium/Selenium.java
@@ -21,69 +21,69 @@ public class Selenium {
     @Value("${hangPwd}")
     private String pwd;
 
-    @Scheduled(cron = "30 59 4 * * *")
-    public void checkOut() throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--no-sandbox");
-        options.addArguments("--headless");
-        options.addArguments("--disable-dev-shm-usage");
-
-        driver = new ChromeDriver(options);
-        driver.get(url);
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        Thread.sleep(2000);
-        driver.switchTo().alert().accept();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        Thread.sleep(2000);
-        driver.findElement(By.className("css-6xo06l")).click();
-        Thread.sleep(100);
-        driver.findElement(By.className("css-1ff1fok")).sendKeys(id);
-        Thread.sleep(100);
-        driver.findElement(By.className("css-zcbjwm")).click();
-        Thread.sleep(100);
-        driver.findElement(By.xpath("//*[@id=\"__next\"]/section/section/section[3]/div[3]/input")).sendKeys(pwd);
-        Thread.sleep(100);
-        driver.findElement(By.className("css-17b9hty")).click();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        Thread.sleep(2000);
-        driver.findElement(By.className("css-307iw5")).click();
-        System.out.println("출석 종료");
-        driver.close();
-        driver.quit();
-    }
-
-    @Scheduled(cron = "0 0 5 * * *")
-    public void checkIn() throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--no-sandbox");
-        options.addArguments("--headless");
-        options.addArguments("--disable-dev-shm-usage");
-
-        driver = new ChromeDriver(options);
-        driver.get(url);
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        Thread.sleep(2000);
-        driver.switchTo().alert().accept();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        Thread.sleep(2000);
-        driver.findElement(By.className("css-6xo06l")).click();
-        Thread.sleep(100);
-        driver.findElement(By.className("css-1ff1fok")).sendKeys(id);
-        Thread.sleep(100);
-        driver.findElement(By.className("css-zcbjwm")).click();
-        Thread.sleep(100);
-        driver.findElement(By.xpath("//*[@id=\"__next\"]/section/section/section[3]/div[3]/input")).sendKeys(pwd);
-        Thread.sleep(100);
-        driver.findElement(By.className("css-17b9hty")).click();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        Thread.sleep(2000);
-        driver.findElement(By.className("css-13067o3")).click();
-        System.out.println("출석 시작");
-        driver.close();
-        driver.quit();
-    }
+//    @Scheduled(cron = "30 59 4 * * *")
+//    public void checkOut() throws InterruptedException {
+//        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+//        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("--no-sandbox");
+//        options.addArguments("--headless");
+//        options.addArguments("--disable-dev-shm-usage");
+//
+//        driver = new ChromeDriver(options);
+//        driver.get(url);
+//        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+//        Thread.sleep(2000);
+//        driver.switchTo().alert().accept();
+//        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+//        Thread.sleep(2000);
+//        driver.findElement(By.className("css-6xo06l")).click();
+//        Thread.sleep(100);
+//        driver.findElement(By.className("css-1ff1fok")).sendKeys(id);
+//        Thread.sleep(100);
+//        driver.findElement(By.className("css-zcbjwm")).click();
+//        Thread.sleep(100);
+//        driver.findElement(By.xpath("//*[@id=\"__next\"]/section/section/section[3]/div[3]/input")).sendKeys(pwd);
+//        Thread.sleep(100);
+//        driver.findElement(By.className("css-17b9hty")).click();
+//        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+//        Thread.sleep(2000);
+//        driver.findElement(By.className("css-307iw5")).click();
+//        System.out.println("출석 종료");
+//        driver.close();
+//        driver.quit();
+//    }
+//
+//    @Scheduled(cron = "0 0 5 * * *")
+//    public void checkIn() throws InterruptedException {
+//        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+//        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("--no-sandbox");
+//        options.addArguments("--headless");
+//        options.addArguments("--disable-dev-shm-usage");
+//
+//        driver = new ChromeDriver(options);
+//        driver.get(url);
+//        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+//        Thread.sleep(2000);
+//        driver.switchTo().alert().accept();
+//        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+//        Thread.sleep(2000);
+//        driver.findElement(By.className("css-6xo06l")).click();
+//        Thread.sleep(100);
+//        driver.findElement(By.className("css-1ff1fok")).sendKeys(id);
+//        Thread.sleep(100);
+//        driver.findElement(By.className("css-zcbjwm")).click();
+//        Thread.sleep(100);
+//        driver.findElement(By.xpath("//*[@id=\"__next\"]/section/section/section[3]/div[3]/input")).sendKeys(pwd);
+//        Thread.sleep(100);
+//        driver.findElement(By.className("css-17b9hty")).click();
+//        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+//        Thread.sleep(2000);
+//        driver.findElement(By.className("css-13067o3")).click();
+//        System.out.println("출석 시작");
+//        driver.close();
+//        driver.quit();
+//    }
 
 //    @Scheduled(cron = "0 1 0 * * *")
 //    public void updateItems() throws InterruptedException {
